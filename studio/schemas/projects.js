51fd5a5da7ab3875ab4841f743ec2a7c48b8d8ad
Andrew Nowacki <anj@Andrews-MacBook-Pro.local>
2023-04-17 10:12:34 -0400

    pushing studio from root

---
+++ studio/schemas/projects.js
@@ -0,0 +1,93 @@
+export default {
+  name: 'projects',
+  title: 'All projects',
+  type: 'document',
+  fields: [
+    {
+      name: "featured",
+      title: "Featured?",
+      description: "Mark this field as checked if you wish this to appear as the featured project. Only one project can be featured at a time, so make sure to disable old 'featured' posts.",
+      type: "boolean",
+      initialValue: false,
+      options: { layout: "checkbox" },
+    }
+    ,
+    {
+      name: "projectName",
+      title: "Project name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      description: 'Image should be 1850x1035 at minimum.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'video',
+      title: 'Video embed',
+      description: 'Paste Amazon S3 storage link to video here.',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Descriptive text',
+      description: 'Descriptive text that will be displayed underneath the image',
+      type: 'string',
+      validation: (Rule) =>
+        Rule.max(200).warning("The description shouldn't be longer than 200 characters"),
+    },
+    {
+      name: "gameMode",
+      title: "Game Mode",
+      description: "Ex: 2v2 Arenas, Competitive",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "client",
+      title: "Client",
+      description: "Ex: National Football League & Enthusiast Gaming",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "date",
+      title: "Date",
+      type: "date",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "code",
+      title: "Code",
+      description: "Ex: 0893-8977-9494",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'imageArray',
+      title: 'More images',
+      type: 'array',
+      description: "Add more images here.",
+      of: [
+        {
+          type: 'image',
+          name: 'additionalImage',
+          title: 'Image',
+        },
+      ],
+    },
+  ],
+
+  preview: {
+    select: {
+      media: 'mainImage',
+      title: 'Landing image',
+    },
+  },
+}
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,5 +3,6 @@ import blockContent from './blockContent'
 import blockContentLimited from './blockContentLimited'
 import faq from './faq'
 import landing from './landing'
+import projects from './projects'
 
-export default [landing, blockContent, blockContentLimited, faq, about]
+export default [landing, blockContent, blockContentLimited, faq, about, projects]
