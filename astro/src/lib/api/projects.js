c4bb8b03dc447812d4b081568c136cfa97b86867
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-04-27 14:36:08 -0400

    pushing our projects section for sulah

--- astro/src/lib/api/projects.js
+++ astro/src/lib/api/projects.js
@@ -12,8 +12,30 @@ export async function getAllProjects() {
       date,
       code,
       "additionalImages": imageArray[].asset->url,
+      "slug": slug.current,
     }
   }`;
   const organizedProjects = await useSanityClient().fetch(query);
   return organizedProjects;
 }
+
+
+export async function getProjectPage() {
+  const query = groq`*[_type == "organizedProjects"][0] {
+    allProjects[]-> {
+      projectName,
+      "imageUrl": mainImage.asset->url,
+      video,
+      description,
+      gameMode, 
+      client,
+      date,
+      code,
+      "additionalImages": imageArray[].asset->url,
+      "slug": slug.current,
+    }
+  }`;
+  const organizedProjects = await useSanityClient().fetch(query);
+  return organizedProjects;
+}
+
