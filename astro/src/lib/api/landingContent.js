18e9d16a0ffd93cc53aff22d6c1ef4b8298dd16c
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-05-09 13:29:08 -0400

    testing landing image

--- astro/src/lib/api/landingContent.js
+++ astro/src/lib/api/landingContent.js
@@ -3,6 +3,7 @@ import { useSanityClient, groq } from 'astro-sanity';
 export async function getLandingContent() {
   const query = groq`*[_type == "landing"][0]{
     "imageUrl": mainImage.asset->url,
+    mainImage,
     "lqip": mainImage.asset->metadata.lqip,
     description,
     "partnerImages": partners[].asset->url,
